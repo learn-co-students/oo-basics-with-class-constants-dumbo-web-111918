064b39c7a8931049625169385b5fff26cec7da18
github username <github email address>
2018-11-24 10:53:14 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,15 @@
+
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  # attr_accessor :genre
+  attr_reader :genre
 
-  def initialize(title)
-    @title = title
+  def initialize(book)
+    @book = book
   end
+GENRES = []
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
   end
-
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,17 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  BRANDS = []
 
-  def initialize(brand)
-    @brand = brand
-  end
+def initialize(brands)
+  @brands = brands
+  if BRANDS.include?(brands)
+    BRANDS
+  else
+  BRANDS << brands
+end
+end
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
 
+def brands
+  @brands
+end
 end
